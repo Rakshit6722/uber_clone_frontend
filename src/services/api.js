c4447501bb21aca359453caa2a742e751cf73398
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-01-03 21:58:53 +0530

    [Rakshit] Refactor: register and login api routes

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 const BASE_URL = import.meta.env.VITE_BASE_URL
 
 export const userApi = {
-    login: `${BASE_URL}/auth/users/login`,
-    register: `${BASE_URL}/auth/users/register`,
+    login: `${BASE_URL}/users/login`,
+    register: `${BASE_URL}/users/register`,
 }
