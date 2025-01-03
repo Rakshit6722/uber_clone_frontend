65c304caa9b8ed47d491fa075e3a63ad32a0f136
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-01-03 22:00:30 +0530

    [Rakshit] Refactor: reconfig user context

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,11 +1,16 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 
 export const UserDataContext = createContext()
 
 const UserContext = ({ children }) => {
-    const user = ''
+    const [user,setUser] = useState({})
+
+    const  value = {
+        user,
+        setUser
+    }
     return (
-        <UserDataContext.Provider value={user}>
+        <UserDataContext.Provider value={value}>
             {children}
         </UserDataContext.Provider>
     )
