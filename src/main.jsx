399dbadac7a4e77bfb3000813b65d4a8f69d1593
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-01-03 21:58:12 +0530

    [Rakshit] Add: config toast

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import UserContext from './context/UserContext.jsx'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserContext>
       <BrowserRouter>
         <App />
+        <ToastContainer position='top-right' />
       </BrowserRouter>
     </UserContext>
   </StrictMode>,
